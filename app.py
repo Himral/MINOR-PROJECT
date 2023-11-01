b489fcb881ba04fc3ecdadac9ce9ddf5ade79dea
Himral <himralgarg@gmail.com>
2023-11-01 18:48:43 +0530

    buttons added
    Signed-off-by: Himral <himralgarg@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
-
+import preprocessor
 st.sidebar.title("Whatsapp Chat analyser")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+    st.dataframe(df)
+
+    #fetch unique users
+    user_list = df['user'].unique.tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"Overall")
+    st.sidebar.selectbox("Show analysis wrt",user_list)
+
+    if(st.sidebar.button("Show analysis"))
 
