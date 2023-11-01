ca29015218c622449d500b55682dc456e0c537dd
Vidhi-20 <vidhirastogi20@gmail.com>
2023-11-01 18:57:29 +0530

    helper file

---
+++ helper.py
@@ -0,0 +1,16 @@
+def fetch_stats(selected_user,df):
+    if selected_user != 'Overall':
+        df = df[df['user']== selected_user]
+    #fetch the number of messages
+
+    num_messages = df.shape[0]
+
+    #fetch no. of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    #fetch total media messages
+    num_media_messages=df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    return num_messages,len(words),num_media_messages
