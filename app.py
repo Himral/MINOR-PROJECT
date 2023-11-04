4b236c0e320f836b73245cb13dceb7100b4b6182
vanshika41 <vanshikajalhotra04@gmail.com>
2023-11-05 02:01:26 +0530

    message

--- app.py
+++ app.py
@@ -53,6 +53,39 @@ if uploaded_file is not None:
             
             with col2:
                 st.dataframe(new_df)
+
+        #monthly timeline
+        st.title("Monthly_Timeline")
+        timeline=helper.monthly_timeline(selected_user,df)
+        fig,ax=plt.subplots()
+        ax.plot(timeline['time'],timeline['message'],color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot('fig')
+
+        #activity map
+        st.title('Activity Map')
+        col1,col2=st.columns(2)
+
+        with col1:
+            st.header("Most Busy Day")
+            busy_day=helper.week_activity_map(selected_user,df)
+            fig,ax=plt.subplots()
+            ax.bar(busy_day.index,busy_day.values,color='purple')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        with col2:
+            st.header("Most Busy Month")
+            busy_month=helper.month_activity_map(selected_user,df)
+            fig,ax=plt.subplots()
+            ax.bar(busy_month.index,busy_month.values,color='orange')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        st.title("Weekly Activity Map")
+        user_heatmap=helper.activity_heatmap(selected_user,df)
+        fig,ax=plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
                 
         #wordcloud
         st.title("Word Cloud")
