2e1cb489b6387dece750d8396e10d59a0e476a28
vanshika41 <vanshikajalhotra04@gmail.com>
2023-11-05 01:33:31 +0530

    message

--- helper.py
+++ helper.py
@@ -117,7 +117,15 @@ def week_activity_map(selected_user,df):
     if selected_user!="Overall":
         df=df[df['user']==selected_user]
 
-    return df['day_name'].value_counts()                               
+    return df['day_name'].value_counts()    
+
+def month_activity_map(selected_user,df):  
+    if selected_user!="Overall":
+        df=df[df['user']==selected_user]
+
+    return df['month'].value_counts()
+
+                
 
    
 
